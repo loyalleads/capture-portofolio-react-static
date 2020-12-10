8508480a6fd776cb86bb865e60ccab0fc27c536a
loyalleads <sherifbutt@Sherifs-iMac-77.local>
2020-12-10 12:05:45 +0000

    first git commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import './app.css'
+import logo from './logo.png'
+
+class App extends Component {
+  render() {
+    
+    
+    return (
+      <>
+        <h1>hi there</h1>
+        <h2>you are the best</h2>
+      </>
+    )
+  }
+}
+
+export default App
