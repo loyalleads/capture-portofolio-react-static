d52e30dced98d216f366d58a58877e40d995af18
loyalleads <sherifbutt@Sherifs-iMac-77.local>
2020-12-11 12:39:21 +0000

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function FaqSection() {
+   return (
+      <div className="faq">
+         <h2>Any Questions <span>FAQ</span></h2>
+         <div className="question">
+            <h4>How Do I Start</h4>
+            <div className="answer">
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium odio perferendis unde rem quisquam alias voluptas a natus? Aliquid inventore aperiam eveniet praesentium perspiciatis distinctio doloribus nam dicta voluptatibus sint?</p>
+            </div>
+         </div>
+         <div className="question">
+            <h4>Daily Schedule</h4>
+            <div className="answer">
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium odio perferendis unde rem quisquam alias voluptas a natus? Aliquid inventore aperiam eveniet praesentium perspiciatis distinctio doloribus nam dicta voluptatibus sint?</p>
+            </div>
+         </div>
+         <div className="question">
+            <h4>How Do I Start</h4>
+            <div className="answer">
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium odio perferendis unde rem quisquam alias voluptas a natus? Aliquid inventore aperiam eveniet praesentium perspiciatis distinctio doloribus nam dicta voluptatibus sint?</p>
+            </div>
+         </div>
+         <div className="question">
+            <h4>How Do I Start</h4>
+            <div className="answer">
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium odio perferendis unde rem quisquam alias voluptas a natus? Aliquid inventore aperiam eveniet praesentium perspiciatis distinctio doloribus nam dicta voluptatibus sint?</p>
+            </div>
+         </div>
+      </div>
+   )
+}
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
 // Page Components
-import AboutSection from '../components/AboutSection'
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection"
 
 const AboutUs = () => {
-   return (
-      
-      <div><AboutSection /></div>
-   );
-}
+  return (
+    <>
+      <div>
+        <AboutSection />
+      </div>
+      <div>
+        <ServicesSection />
+        </div>
+        <div>
+           <FaqSection/>
+        </div>
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
